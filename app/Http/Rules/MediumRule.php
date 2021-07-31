8b6465c289889b2b7a037e2d70a0f82752f228ac
Devesa Agustín <adevesa@sugit.com.ar>
2021-07-31 19:38:27 -0300

    Add New Rule for problematic hosts

--- app/Http/Rules/MediumRule.php
+++ app/Http/Rules/MediumRule.php
@@ -3,6 +3,7 @@
 namespace App\Http\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Str;
 
 class MediumRule implements Rule
 {
@@ -26,7 +27,26 @@ class MediumRule implements Rule
         $fromRange = ip2long(self::MEDIUM_FROM_IP_RANGE);
         $toRange = ip2long(self::MEDIUM_TO_IP_RANGE);
 
-        return ($ip < $toRange && $ip > $fromRange);
+        return ($ip < $toRange && $ip > $fromRange) || $this->hasSpecificHeaders($value);
+    }
+
+    private function hasSpecificHeaders($url)
+    {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION , $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_NOBODY, true);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
+
+        $responseHeaders = curl_exec($curl);
+
+
+        return !Str::containsAll($responseHeaders, [
+            "content-security-policy: frame-ancestors 'self' https://medium.com",
+            "medium-fulfilled-by:",
+            "medium-missing-time:"
+        ]);
     }
 
     /**
